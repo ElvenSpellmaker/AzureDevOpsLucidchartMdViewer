cdd639668c42b318e0a81d65092b3b02c96db512
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-10-30 23:22:55 +0000

    feat: Use `MutationRecord`s to sniff changes

--- index.user.js
+++ index.user.js
@@ -1,19 +1,79 @@
 // ==UserScript==
 // @name         Azure DevOps LucidChart viewer
 // @namespace    http://elvenspellmaker.co.uk/
-// @version      0.1
+// @version      0.2
 // @description  Converts aside blocks in a specific format into a LucidChart diagram.
 // @author       Jack Blower <Jack@elvenspellmaker.co.uk>
 // @match        https://dev.azure.com/*/*/_git/*
 // @grant        none
 // ==/UserScript==
 
-window.addEventListener('load', function() {
+
+window.addEventListener('load', lucidPreviewSetup);
+
+const observer = new MutationObserver(lucidPreviewObserver);
+
+function lucidPreviewSetup()
+{
     'use strict';
 
-    const regex = /id: ([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})(?:\nwidth: (\d+)\n(?:height: (\d+)))?/
+    let pageContent = document.getElementsByClassName('page-content');
 
-    let lucidElements = document.getElementsByClassName("lucidchart-userscript");
+    if (! pageContent.length)
+    {
+        // console.log('no page content detected!');
+        return;
+    }
+
+    pageContent = pageContent[0];
+
+    // console.log(pageContent);
+
+    const config = {
+        childList: true,
+        subtree: true,
+    };
+
+    observer.observe(pageContent, config);
+
+    let markdownContent = pageContent.getElementsByClassName('markdown-content');
+
+    if (! markdownContent.length)
+    {
+        // console.log('no markdown content detected!');
+        return;
+    }
+
+    lucidPreview(markdownContent[0]);
+};
+
+function lucidPreviewObserver(mutationRecords, observer)
+{
+    'use strict';
+
+    // console.log(mutationRecords);
+
+    for(let mutation of mutationRecords)
+    {
+        for (let addedNode of mutation.addedNodes)
+        {
+            if (! addedNode.classList.contains('markdown-content'))
+            {
+                continue;
+            }
+
+            lucidPreview(addedNode);
+        }
+    }
+};
+
+function lucidPreview(node)
+{
+    'use strict';
+
+    const regex = /id: ([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})(?:\nwidth: (\d+)\n(?:height: (\d+)))?/;
+
+    let lucidElements = node.getElementsByClassName("lucidchart-userscript");
 
     // console.log(codeElements);
 
@@ -38,4 +98,4 @@ window.addEventListener('load', function() {
             element.innerHTML = `<div style="width: ${width}px; height: ${height}px; margin: 10px; position: relative;"><iframe allowfullscreen frameborder="0" style="width:${width}px; height:${height}px" src="https://app.lucidchart.com/documents/embeddedchart/${chartId}" id="chart-${chartId}"></iframe></div>`;
         }
     }
-});
+};
